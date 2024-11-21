56391cbd37c7219406979231c7ff73277fb7336f
Nabeel Shakeel <nabeel.shakeel@emumba.com>
2024-11-21 17:13:00 +0500

    chore: add theme variable settings in provider

--- src/providers/app-providers.tsx
+++ src/providers/app-providers.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material';
 import { store } from '../redux/store';
-import { theme } from '../theme';
+import { theme, CSSVariables } from '../theme';
 interface AppProvidersProps {
   children: React.ReactNode;
 }
@@ -12,7 +12,10 @@ export function AppProviders({ children }: AppProvidersProps) {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <CSSVariables theme={theme} />
+          {children}
+        </ThemeProvider>
       </Provider>
     </BrowserRouter>
   );
