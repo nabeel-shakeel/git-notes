6ebaa47d048072be668c00ded598af6a5a4b909d
Nabeel Shakeel <nabeel.shakeel@emumba.com>
2024-11-21 17:17:26 +0500

    feat: user gist details UI

---
+++ src/features/profile/components/user-gists/index.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Stack, Typography } from '@mui/material';
+import {
+  Pagination,
+  GistGridLoading,
+  ErrorComponent,
+} from '../../../../components';
+import { routes } from '../../../../routing/routes';
+import {
+  useGetUserGistsQuery,
+  useGetStarredGistsQuery,
+} from '../../../gists/gistsApiSlice';
+import { GistCard } from '../../../gists/components/gist-card';
+import {
+  USER_GISTS_PER_PAGE,
+  USER_GISTS_PER_PAGE_OPTIONS,
+} from '../../../../utils/constants';
+import './user-gists.styles.scss';
+
+interface UserGistsProps {
+  gistType: 'all' | 'starred';
+}
+
+export function UserGists({ gistType }: UserGistsProps) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(USER_GISTS_PER_PAGE);
+
+  const navigate = useNavigate();
+
+  const {
+    data: allGists = [],
+    isLoading: isLoadingAllGists,
+    error: errorAllGists,
+  } = useGetUserGistsQuery(
+    {
+      page: page + 1,
+      per_page: rowsPerPage,
+    },
+    { skip: gistType === 'starred' }
+  );
+
+  const {
+    data: starrtedGists = [],
+    isLoading: isLoadingStarredGists,
+    error: errorStarredGists,
+  } = useGetStarredGistsQuery(
+    {
+      page: page + 1,
+      per_page: rowsPerPage,
+    },
+    { skip: gistType === 'all' }
+  );
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleGistClick = (id: string) => {
+    navigate(routes.GIST.replace(':id', id));
+  };
+
+  if (isLoadingAllGists || isLoadingStarredGists)
+    return <GistGridLoading items={8} />;
+
+  if (errorAllGists || errorStarredGists) return <ErrorComponent />;
+
+  const pageTitle = gistType === 'all' ? 'User Gists' : 'Starred Gists';
+  const gistsCount =
+    gistType === 'all' ? allGists.length : starrtedGists.length;
+  const selectedGists = gistType === 'all' ? allGists : starrtedGists;
+
+  return (
+    <Stack spacing={4}>
+      <Stack direction="row" gap={1} alignItems="center">
+        <Typography variant="h5" color="secondary">
+          {pageTitle}
+        </Typography>
+        <Typography variant="body2" className="gist-count">
+          {gistsCount}
+        </Typography>
+      </Stack>
+      {selectedGists.map((gist) => (
+        <GistCard gist={gist} />
+      ))}
+      <Pagination
+        count={-1}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        rowsPerPageOptions={USER_GISTS_PER_PAGE_OPTIONS}
+      />
+    </Stack>
+  );
+}
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,49 @@
+import { Grid2 as Grid, Box, Avatar, Typography, Button } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { useAppSelector } from '../../redux/hooks';
+import { UserGists } from '../../features/profile/components/user-gists';
+import './profile.styles.scss';
+
 export function ProfilePage() {
-  return <h1>Profile Page</h1>;
+  const { state } = useLocation();
+  const user = useAppSelector((state) => state.auth.user);
+
+  return (
+    <Grid container spacing={{ xs: 6 }}>
+      <Grid size={{ xs: 12, md: 3 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 4,
+          }}
+        >
+          <Avatar
+            src={user?.photoURL || ''}
+            alt="profile-picture"
+            className="profile-avatar"
+          />
+          <Typography variant="h5" color="#3D3D3D">
+            {user?.displayName}
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ textTransform: 'none' }}
+            component="a"
+            href={user?.profileURL || ''}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View GitHub Profile
+          </Button>
+        </Box>
+      </Grid>
+      <Grid size={{ xs: 12, md: 7 }}>
+        <UserGists gistType={state.gistType} />
+      </Grid>
+    </Grid>
+  );
 }
